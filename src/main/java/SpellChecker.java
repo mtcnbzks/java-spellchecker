bc35fff16508cdf6c2980c8527ffafb6e9e94697
mtcnbzks <mtcnbzks@gmail.com>
2023-04-26 17:23:09 +0300

    imports deleted

--- src/main/java/SpellChecker.java
+++ src/main/java/SpellChecker.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SpellChecker {
    public static void main(String[] args) {
